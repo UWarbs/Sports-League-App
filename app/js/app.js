d30470b87c8e3a800dcecebee8b68d3abc56cbbc
Kyle Warbis <kyle.warbis@gmail.com>
2014-06-12 16:23:54 -0700

    added win/loss counter

--- app/js/app.js
+++ app/js/app.js
@@ -3,28 +3,37 @@ var angular = require('angular');
 (function(){
 	var app = angular.module('pingPong', []);
 
+	var players = {
+		Kyle:
+		{
+			wins: 0,
+			losses:0
+		},
+		Kevin:
+		{
+			wins:0,
+			losses:0
+		},
+		SwampGuzller:
+		{
+			wins: 0,
+			losses: 0
+		},
+		Jeff:{
+			wins:0,
+			losses: 0
+		}
+		};
+	app.controller('StandingController', function($scope){
+		$scope.players = players;
 
-	app.controller('StandingController', function(){
-		this.players = people;
+
+		$scope.addLoss = function(name){
+			return $scope.players[name].losses++;
+		};
+		$scope.addWin = function(name){
+			return $scope.players[name].wins++;
+		};
+		$scope.orderProp = 'player.record';
 	});
-
-	var people = [
-	{
-		name: "Kyle",
-		wins: 0,
-		losses:0
-	},
-	{
-		name:"Swampguzzler",
-		wins:0,
-		losses:0
-	},
-	{
-		name: "Kevin",
-		wins: 0,
-		losses: 0
-	}
-	];
-
-
 })();
