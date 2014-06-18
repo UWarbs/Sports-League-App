ae964d2182e89042fce06482e8c630e9a35b2b0c
Kyle Warbis <kyle.warbis@gmail.com>
2014-06-18 13:28:33 -0700

    rewriting to make more scalable

--- app/js/app.js
+++ app/js/app.js
@@ -2,29 +2,74 @@ var angular = require('angular');
 
 (function(){
 	var app = angular.module('pingPong', []);
+	app.service('PlayerService', function(){
+		var uId = 1;
+		var players = [{
+			id: 0,
+			'name': 'Kyle',
+			'wins': 0,
+			'losses':0
+		}];
 
-	var players = {
-		Kyle:
-		{
-			wins: 0,
-			losses:0
-		},
-		Kevin:
-		{
-			wins:0,
-			losses:0
-		},
-		SwampGuzller:
-		{
-			wins: 0,
-			losses: 0
-		},
-		Jeff:{
-			wins:0,
-			losses: 0
-		}
+		this.save = function(player){
+			if(!player.id){
+				player.id = uId++;
+				players.push(player);
+			}else{
+				for(var i = 0; i <= players.length; i++){
+					if(players[i].id == player.id){
+						players[i] = player;
+					}
+				}
+			}
+		}; //this.save
+
+		//searches player list for given id returns player object if found
+		this.get = function(id){
+			for(var i = 0; i <= players.length; i++){
+				if(players[i].id == id){
+					return players[i];
+				}
+			}
+		}; //this.get
+
+		//delete player if found
+		this.delete = function(id){
+			for(var i = 0; i <= players.length; i++){
+				if(players[i].id == id){
+					players.splice(i, 1);
+				}
+			}
+		}; //this.delete
+
+		//returns contact list
+		this.list = function(){
+			return players;
 		};
-	app.controller('StandingController', function($scope){
+	});
+
+	
+	app.controller('PlayerController', function($scope, PlayerService){
+		$scope.players = PlayerService.list();
+		$scope.newplayer = {};
+
+		$scope.savePlayer = function(){
+			console.log($scope);
+			PlayerService.save($scope.newplayer);
+			$scope.newplayer = {};
+		};
+
+		$scope.delete = function(id){
+			if($scope.newplayer.id == id) $scope.newplayer = {};
+		};
+
+		$scope.edit = function(id){
+			$scope.newplayer = angular.copy(PlayerService.get(id));
+		}; 
+	});
+
+
+	/*app.controller('StandingController', function($scope){
 		$scope.players = players;
 
 
@@ -35,5 +80,8 @@ var angular = require('angular');
 			return $scope.players[name].wins++;
 		};
 		$scope.orderProp = 'player.record';
-	});
+	});*/
 })();
+
+//when updating wins/losses, need to do a db.players.update({name: "playerName"}, {$inc: {wins: 1}})
+//automatically creates "win" field in database if none exists
